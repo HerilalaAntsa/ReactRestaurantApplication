a71db3bb599d0012676364868d398fb9a4c95c7b
HerilalaAntsa <herilala.antsa@gmail.com>
2019-04-05 10:13:00 +0300

    app

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
+
+const Firebase = () => (
+  <div>
+    <h1>App</h1>
+  </div>
+);
+
+export default Firebase;
+
+export { FirebaseContext };
