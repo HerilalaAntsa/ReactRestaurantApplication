e328aa5949e012f29ba18364a00294103217203e
HenintsoaAdri <henintsoa.adri@hotmail.com>
2019-04-05 11:35:57 +0300

    firebase config

--- src/constants/base.js
+++ src/constants/base.js
@@ -3,9 +3,9 @@ import Rebase from 're-base';
 const base = Rebase.createClass({
 // copier les 3 premières lignes de VOTRE config de votre base !!!
  // NE PRENEZ PAS LES VALEURS QUI SONT ICI !!!
-    apiKey: "AIzaSyCEZkm6H0sVZySxffOfO0HAmg7pzHLHqnA",
-    authDomain: "hobbies-15e06.firebaseapp.com",
-    databaseURL: "https://hobbies-15e06.firebaseio.com" // pas de , à la fin
+ apiKey: "AIzaSyA4BVn7dJPxO3u-KXfgh2W1kr7ZGc0kSZc",
+ authDomain: "exo-restaurant.firebaseapp.com",
+ databaseURL: "https://exo-restaurant.firebaseio.com" // pas de , à la fin
 });
 
 
