74cd75062ab808a6bac99a920274ffeb500d1f7d
HerilalaAntsa <herilala.antsa@gmail.com>
2019-04-08 18:34:13 +0300

    app antsa

--- src/components/Accueil/RestaurantList.js
+++ src/components/Accueil/RestaurantList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RestaurantList(props){
     return (
-        <li onClick={() => props.removeHobby(props.cle)} index={props.index}>{props.name}</li>
+        <li onClick={} index={props.index}>{props.name}</li>
     )
 }
 export default RestaurantList
--- src/components/RestaurantDetails/index.js
+++ src/components/RestaurantDetails/index.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Hobby from './components/Hobby';
-import base from './base';
+import RestaurantList from './RestaurantList';
+import base from '../../constants/base';
 
-class Restaurant extends Component {
+class RestaurantDetails extends Component {
   constructor(props){
     super(props);
-    this.state = { hobbies: ['Football', 'Basket', 'Chant'] }
-  }
-  removeHobby(key) {
-    const copieHobbies = {...this.state.hobbies}; // spread operator permert de cloner des object
-    //delete copieHobbies[key]
-    copieHobbies[key] = null;
-    this.setState({
-      hobbies:copieHobbies
-    })
-	}
-  
-  
-  addHobby() {
-    const copieHobbies = {...this.state.hobbies}; // spread operator permert de cloner des object
+    this.state = { restaurantList: [] }
+  } 
     
-    copieHobbies['hobby' + Math.random()] = this.input.value;
-		this.setState({
-			hobbies: copieHobbies
-    });
-    console.log("Hobbies added" + this.state.hobbies);
-    this.input.value='';
-	}
-  
   changeInputMessage(event) {
     
 		this.setState({
@@ -38,45 +16,38 @@ class Restaurant extends Component {
   }
   
   componentWillMount() {
-    //alert("toto")
+    console.log("Will mount")
     // this runs right before the <App> is rendered
-    this.ref = base.syncState("hobbies", {
+    this.ref = base.syncState("restaurantList", {
       context: this,
-      state: 'hobbies'
+      state: 'restaurantList'
     });
   }
-  
+
   componentWillUnmount() {
-    base.removeBinding(this.ref);
+      console.log("Will unmount");
+      base.removeBinding(this.ref);
   }
 
   render() {
-    let listHobbies = Object.keys(this.state.hobbies).map((key,index) => {
-      let el = this.state.hobbies[key];
-      return <Hobby key = {index}
-                    cle = {key}
-                    name={el}
-                    removeHobby={this.removeHobby.bind(this)}>
-              {el}
-             </Hobby>
+    let listRestaurant = this.state.restaurantList.map((restaurant) => {
+      return <RestaurantList key = {restaurant._id}
+                    cle = {restaurant.index}
+                    name={restaurant.nom}>
+             </RestaurantList>
     })
     return (
-      <div className="App">
+      <div className="Accueil">
       <div>
-        <h2>Liste des hobbies</h2>
-        Nombre de hobbies : {listHobbies.length}
+        <h2>Liste des restaurant</h2>
+        Nombre de restaurant : {listRestaurant.length}
           <ul>
-        {listHobbies}
+        {listRestaurant}
         </ul>
-        <input
-                ref={(input) => this.input = input}
-                type="text" 
-                placeholder="Ecrivez une hobby .."/>
-        <button onClick={() => this.addHobby()}>Nouveau Hobby</button>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default RestaurantDetails;
